a300e4c3d0ff82e12f785a34be605ad1a71f18c3
Yoannis Sanchez Soto <yossthedev@gmail.com>
2026-04-15 19:37:58 -0400

    fix: rezisable panels

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import * as React from "react"
 import * as ResizablePrimitive from "react-resizable-panels"
 
 import { cn } from "@/components/lib/utils"
@@ -38,7 +40,7 @@ function ResizableHandle({
       )}
       {...props}
     >
-      {withHandle && (
+      {(withHandle ?? false) && (
         <div className="z-10 flex h-6 w-1 shrink-0 rounded-lg bg-border" />
       )}
     </ResizablePrimitive.Separator>
