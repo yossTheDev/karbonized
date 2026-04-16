26b330e463c21c6fdf6ac816724156c55fee85f7
Yoannis Sanchez Soto <yossthedev@gmail.com>
2026-04-15 21:26:06 -0400

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -47,8 +47,10 @@ export default [
 				{ allowConstantExport: true },
 			],
 			'prettier/prettier': 'error',
-			'arrow-body-style': 'off',
-			'prefer-arrow-callback': 'off',
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
 		},
 		settings: {
 			react: {
